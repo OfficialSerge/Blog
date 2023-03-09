75fb4e3a7beeca2e93fc7157f08dd769484e8a2f
Sergiu Pod <sergiupod@gmail.com>
2023-03-08 19:23:08 -0600

    fix error

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,7 +1,7 @@
 import { defineCliConfig } from "sanity/cli";
 
-let projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!; // "pv8y60vp"
-let dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!; // "production"
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!; // "pv8y60vp"
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!; // "production"
 
 export default defineCliConfig({
   api: {
